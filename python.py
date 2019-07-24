c924edd24c5d31de826d2646a7f2969af63511fc
nataliagomes <naty.pinheiro95@gmail>
2019-07-23 23:18:10 -0300

    fazerexercicios

---
+++ python.py
@@ -0,0 +1,93 @@
+classe  Pessoa :
+    def  __init__ ( self , nome , idade ):
+        self .nome = nome
+        auto .idade = idade
+ 
+    def  get_nome ( self ):
+        return auto .nome
+     
+    def  get_idade ( self ):
+        return  auto .
+
+        
+
+classe  Pet :
+    def  __init__ ( self , pet_nome , nome_dono , data_nascimento ):
+        self .pet_nome = pet_nome
+        self .nome_dono = nome_dono
+        self .data_nascimento = data_nascimento
+    def  get_pet_nome ( self ):
+        return  self .pet_nome
+    def  get_nome_dono ( self ):
+        return  auto .nome_dono
+    def  get_data_nascimento ( self ):
+        devolver  auto .data_nascimento
+
+
+pessoa = Pessoa ( ' Natalia ' , ' 23 ' )
+
+print (pessoa.get_nome (), pessoa.get_idade ())
+
+animais = Pet ( ' Cacau ' , ' Geane ' , ' 2018/02/1 ' )
+print (animal.get_pet_nome (), animal.get_nome_dono (), animal.get_data_nascimento ())classe  Pessoa :
+    def  __init__ ( self , nome , idade ):
+        self .nome = nome
+        auto .idade = idade
+ 
+    def  get_nome ( self ):
+        return  auto .nome
+     
+    def  get_idade ( self ):
+        return auto .
+
+classe  Pet :
+    def  __init__ ( self , pet_nome , nome_dono , data_nascimento ):
+        self .pet_nome = pet_nome
+        self .nome_dono = nome_dono
+        self .data_nascimento = data_nascimento
+    def  get_pet_nome ( self ):
+        return  self .pet_nome
+    def  get_nome_dono ( self ):
+        return auto .nome_dono
+    def  get_data_nascimento ( self ):
+        devolver  auto .data_nascimento
+
+classe  Pessoa :
+    def  __init__ ( self , nome , idade ):
+        self .nome = nome
+        auto .idade = idade
+ 
+    def  get_nome ( self ):
+        return auto .nome
+     
+    def  get_idade ( self ):
+        return  auto .
+
+classe  Pet :
+    def  __init__ ( self , pet_nome , nome_dono , data_nascimento ):
+        self .pet_nome = pet_nome
+        self .nome_dono = nome_dono
+        self .data_nascimento = data_nascimento
+    def  get_pet_nome ( self ):
+        return  self .pet_nome
+    def  get_nome_dono ( self ):
+        return  auto .nome_dono
+    def  get_data_nascimento ( self ):
+        devolver  auto .data_nascimento
+
+
+pessoa = Pessoa ( ' Natalia ' , ' 23 ' )
+
+print (pessoa.get_nome (), pessoa.get_idade ())
+
+animais = Pet ( ' Cacau ' , ' geane ' , ' 2018/11/18 ' )
+print (animal.get_pet_nome (), animal.get_nome_dono (), animal.get_data_nascimento ())classe  Pessoa :
+    def  __init__ ( self , nome , idade ):
+        self .nome = nome
+        auto .idade = idade
+ 
+    def  get_nome ( self ):
+        return  auto .nome
+     
+    def  get_idade ( self ):
+        return auto .
